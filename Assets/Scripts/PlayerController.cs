377f7a7fe2a51e4f85bbf427dc3bc212332e31b3
Jan <janjilecek@fgmail.com>
2020-05-20 19:20:56 +0200

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 3;
+    
+
+    public Animator animator;
+    
+    // camera and rotation
+    public Transform cameraHolder;
+    public float mouseSensitivity = 2f;
+    public float upLimit = -50;
+    public float downLimit = 50;
+    
+    // gravity
+    private float gravity = 9.87f;
+    private float verticalSpeed = 0;
+    
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        float verticalRotation = Input.GetAxis("Mouse Y");
+        
+        transform.Rotate(0, horizontalRotation * mouseSensitivity, 0);
+        cameraHolder.Rotate(-verticalRotation*mouseSensitivity,0,0);
+
+        Vector3 currentRotation = cameraHolder.localEulerAngles;
+        if (currentRotation.x > 180) currentRotation.x -= 360;
+        currentRotation.x = Mathf.Clamp(currentRotation.x, upLimit, downLimit);
+        cameraHolder.localRotation = Quaternion.Euler(currentRotation);
+    }
+
+    private void Move()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        if (characterController.isGrounded) verticalSpeed = 0;
+        else verticalSpeed -= gravity * Time.deltaTime;
+        Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
+        
+        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+        characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
+        
+        animator.SetBool("isWalking", verticalMove != 0 || horizontalMove != 0);
+    }
+}
