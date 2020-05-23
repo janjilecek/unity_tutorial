74d19d3cf5feaa89d5ba731c826d4ba2ab06b4c8
Jan <janjilecek@fgmail.com>
2020-05-23 19:21:23 +0200

    Horde

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent pathfinder;
+    private Transform target;
+    
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+    }
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour
+{
+    public Enemy enemy;
+    private List<Enemy> enemies;
+    
+    [Range (0,100)]
+    public int numberOfEnemies = 25;
+    private float range = 70.0f;
+    
+    void Start()
+    {
+        enemies = new List<Enemy>(); // init as type
+        for (int index = 0; index < numberOfEnemies; index++)
+        {
+            Enemy spawned = Instantiate(enemy, RandomNavmeshLocation(range), Quaternion.identity) as Enemy;
+            enemies.Add(spawned);
+        }
+    }
+    
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+}
