120ec8284fd29b344e0da3d746a9400343a3d719
Jan <janjilecek@fgmail.com>
2020-05-28 19:49:00 +0200

    limbo menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
